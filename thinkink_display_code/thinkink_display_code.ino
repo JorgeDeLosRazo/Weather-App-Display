b5f51050aea946a53a950dbf55c563b57f3d1e66
Jorge Razo <delos3@pdx.edu>
2025-08-23 04:02:17 -0700

    Code for display

---
+++ thinkink_display_code/thinkink_display_code.ino
@@ -0,0 +1,48 @@
+
+#include "Adafruit_ThinkInk.h"
+
+#define EPD_DC 33
+#define EPD_CS 15
+#define EPD_BUSY 32 // can set to -1 to not use a pin (will wait a fixed delay)
+#define SRAM_CS 21
+#define EPD_RESET 27  // can set to -1 and share with microcontroller Reset!
+#define EPD_SPI &SPI // primary SPI
+
+ThinkInk_370_Mono_BAAMFGN display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, EPD_SPI);
+
+void setup() {
+  Serial.begin(115200);
+
+  while (!Serial) { //This waits until connection to the serial is made
+    delay(10);
+  }
+  Serial.println("Hello dumbass");
+  display.begin(THINKINK_MONO); //Initializes the display as a monohrome ThinkInk display
+}
+
+void loop() {
+  display.clearBuffer();
+  display.setTextSize(3);
+  display.setCursor(20, 20);
+  display.println("Insert City");
+
+  display.setTextSize(4);
+  display.setCursor(20, 80);
+  display.print("22");
+  display.drawCircle(display.getCursorX() , display.getCursorY() - 3, 3, EPD_BLACK);
+  display.println(" C");
+
+  display.setTextSize(2);
+  display.setCursor(20, 140);
+  display.println("Humidity: 60%");
+  display.setCursor(20, 170);
+  display.println("Clear");
+
+  display.setTextSize(1);
+  display.setCursor(20, 210);
+  display.println("Updated: 10:15 AM");
+
+  display.display();
+  delay(10000);
+
+}
